2c54330b3096bbd563ee45fd62402030c53da792
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-05-27 22:23:52 +0530

    Create Food.jsx

---
+++ Day1/Food.jsx
@@ -0,0 +1,12 @@
+export default function Food() {
+    const food1 = "Orange";
+    const food2 = "Apple";
+
+    return (
+        <>
+            <li>Banana</li>
+            <li>{food1}</li>
+            <li>{food2}</li>
+        </>
+    )
+}
