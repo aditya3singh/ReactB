637a513289c3520acc518201334f11b4e3fec920
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-05-27 22:27:08 +0530

    Add files via upload

---
+++ Day1/App.jsx
@@ -0,0 +1,17 @@
+
+import Food from './Food'
+import Footer from './Footer'
+import Header from './Header'
+
+export default function App() {
+
+  return (
+    <>
+      <Header/>
+      <Food />
+      <Footer/>
+    </>
+  )
+}
+
+ 
