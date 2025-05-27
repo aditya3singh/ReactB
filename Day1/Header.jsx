3d8cb5d1c5b80fe1d7ed562d5f004d4f0c29aeb5
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-05-27 22:24:40 +0530

    Add files via upload

---
+++ Day1/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <header>
+            <h1>ASG</h1>
+            <nav>
+                <ul>
+                    <li><a href="#">home</a></li>
+                    <li><a href="#">about</a></li>
+                    <li><a href="#">contect</a></li>
+                    <li><a href="#">Services</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    );
+}
