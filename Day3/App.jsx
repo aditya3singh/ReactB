e3e1fe4a2d5345064f40b1d4901581f3f4c568e9
Aditya Singh Gautam <119742213+aditya3singh@users.noreply.github.com>
2025-05-30 23:42:47 +0530

    Add files via upload

---
+++ Day3/App.jsx
@@ -0,0 +1,10 @@
+import ConditionalRender2 from "./ConditionalRender2";
+
+export default function App() {
+  
+  return (
+    <>
+    <ConditionalRender2 isLoggedIn={true} username="Brother" />
+    </>
+  );
+}
